3361f2046db1bf3f075a21766812fce40f194b47
rasulbarak <rasul.barak@sabanciuniv.edu>
2025-07-08 00:08:18 +0300

    add casting function for wizard

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ class Wizard
     public string name;
     public int age;
     public string favSpell;
+    public int health = 100;
     public int mana = 2;
     public float experience = 0;
 
@@ -20,7 +21,24 @@ class Wizard
         name = _name;
         age = _age;
         favSpell = _favSpell;
-    } 
+    }
+
+    public void castSpellAtOpp(Wizard opponent)
+    /*
+        Casts a spell on an opponent if given. Will decrease the health of the opponent.
+    */
+    {
+        Console.WriteLine(name + " casts " + favSpell + " at " + opponent.name + ".");
+        experience += 0.3f;
+        opponent.health -= 20;
+        checkHealth(opponent);
+    }
+
+    public void checkHealth(Wizard opponent)
+    // checks health of the Wizard that has taken damage
+    {
+        
+    }
 }
 
 class Program
