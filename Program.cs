c5245cefc07cbf0fee612804ca5ffc0c1683f341
rasulbarak <rasul.barak@sabanciuniv.edu>
2025-07-07 23:48:18 +0300

    add data members and constructor for Wizard class

--- Program.cs
+++ Program.cs
@@ -1,4 +1,27 @@
-﻿namespace classes3;
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace classes3;
+
+class Wizard
+{
+    // data members
+    public string name;
+    public int age;
+    public string favSpell;
+    public int mana = 2;
+    public float experience = 0;
+
+    // member functions
+    public Wizard(string _name, int _age, string _favSpell)
+    /*  
+        This is our parametric constructor. No default constructor exists. This is the only one.
+    */
+    {
+        name = _name;
+        age = _age;
+        favSpell = _favSpell;
+    } 
+}
 
 class Program
 {
